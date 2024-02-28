6959ed4c2bebb5032399edfc3945c6a6617f7314
Johan Caicedo <camilo.design07@gmail.com>
2024-02-28 16:26:04 -0500

    Primeras modificaciones de la plantilla Hero section and mipagina (test)

---
+++ src/pages/mipagina.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Page useSplashScreenAnimation>
+      <HeroSection sectionId="hero" />
+       <p>Hola la pagina</p>
+      </Page>
+    </>
+  );
+}
