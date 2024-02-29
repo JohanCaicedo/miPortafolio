1fe839621783df14645375e31e15a646bd9f0649
Johan Caicedo <camilo.design07@gmail.com>
2024-02-29 12:21:30 -0500

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,16 @@
 module.exports = {
 
   pathPrefix:"/miPortafolio",
-  plugins: [
+  
+  
+  plugins: [    
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
+          siteName: "JC Portafolio de diseño", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
@@ -20,11 +22,11 @@ module.exports = {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
+        googleAnalytics: {
+             trackingId: "G-1TTYPPV0JL",
+             anonymize: true, // Default true
+             environments: ["production", "development"] // Default ["production"]
+        }
       },
     },
   ],
